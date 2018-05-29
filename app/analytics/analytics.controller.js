f8394e3149fa9d37e71bca19fba3a098df1ed638
kr3037 <kr3037@student.uni-lj.si>
2018-05-29 10:42:51 +0200

    Dodan tab za WIP Violations v Analytics

--- app/analytics/analytics.controller.js
+++ app/analytics/analytics.controller.js
@@ -5,24 +5,24 @@
         .module('app')
         .controller('AnalyticsController', AnalyticsController);
 
-    AnalyticsController.$inject = ['ProjectsService','CardsService', '$rootScope', '$scope', '$location', 'ModalProvider'];
+    AnalyticsController.$inject = ['ProjectsService', 'CardsService', '$rootScope', '$scope', '$location', 'ModalProvider'];
 
-    function AnalyticsController(ProjectsService,CardsService, $rootScope, $scope, $location, ModalProvider) {
+    function AnalyticsController(ProjectsService, CardsService, $rootScope, $scope, $location, ModalProvider) {
         var vm = this;
         vm.labels = [];
         vm.cardLeadTime = [];
         vm.leadTimeAvg = 0;
-        ProjectsService.getProjects().then(function(result) {
-            if(result.status === 200) {
+        ProjectsService.getProjects().then(function (result) {
+            if (result.status === 200) {
                 vm.projects = result.data;
             }
         });
-        CardsService.getAllCardTypes().then(function(result) {
-            if(result.status === 200) {
+        CardsService.getAllCardTypes().then(function (result) {
+            if (result.status === 200) {
                 vm.cardTypes = result.data;
             }
         });
-        vm.baseChart = { 
+        vm.baseChart = {
             "chart": {
                 "caption": "Card lead time",
                 "subcaption": "",
@@ -33,12 +33,12 @@
             ,
             "categories": [
                 {
-                    "category":{
+                    "category": {
                         "label": "1"
                     }
                 }
             ],
-            "dataset":[{
+            "dataset": [{
                 "seriesname": "None",
                 "renderas": "area",
                 "data": [
@@ -50,10 +50,10 @@
 
         vm.dataSource = angular.copy(vm.baseChart);
         vm.dataSource2 = angular.copy(vm.baseChart);
-        vm.reset = function(){
+        vm.reset = function () {
             vm.min = 0;
             vm.max = 100;
-            vm.options={
+            vm.options = {
                 projects: '',
                 startCreation: '',
                 endCreation: '',
@@ -61,44 +61,44 @@
                 endFinished: '',
                 startDevelopment: '',
                 endDevelopment: '',
-                type:-1,
+                type: -1,
                 fromSize: '',
-                toSize:''
+                toSize: ''
             };
             console.log(vm.options);
         };
         vm.reset();
         vm.defOptions = angular.copy(vm.options);
         vm.popup = {
-             open1: false,
-             open2: false,
-             open3: false,
-             open4: false,
-             open5: false,
-             open6: false
+            open1: false,
+            open2: false,
+            open3: false,
+            open4: false,
+            open5: false,
+            open6: false
         };
-        
-        
+
+
         vm.startOptions = {
             maxDate: new Date()
         };
         vm.endOptions = {
             minDate: new Date()
         };
-        vm.setMin = function() {
+        vm.setMin = function () {
             vm.min = vm.options.fromSize;
-            if(vm.options.toSize < vm.min)
+            if (vm.options.toSize < vm.min)
                 vm.options.toSize = vm.min;
         };
-        vm.setMax = function() {
+        vm.setMax = function () {
             vm.max = vm.options.toSize;
-            if(vm.options.fromSize > vm.max)
+            if (vm.options.fromSize > vm.max)
                 vm.options.fromSize = vm.max;
         };
-       vm.updateGraph1 = function(){
-            var newDataSource = angular.copy(vm.baseChart); 
-            newDataSource.dataset =vm.dataset2;
-            newDataSource.categories[0].category =vm.category;
+        vm.updateGraph1 = function () {
+            var newDataSource = angular.copy(vm.baseChart);
+            newDataSource.dataset = vm.dataset2;
+            newDataSource.categories[0].category = vm.category;
             newDataSource.trendlines = [
                 {
                     "line": [
@@ -115,9 +115,9 @@
 
             vm.dataSource = newDataSource;
         };
-        vm.updateGraph2 = function(){
+        vm.updateGraph2 = function () {
             vm.dataSource = angular.copy(vm.baseChart);
-            
+
         };
         /*
         FusionCharts['debugger'].outputTo(function (message) {
@@ -125,55 +125,65 @@
         });
         FusionCharts['debugger'].enable(true);
         */
-       vm.setIndex = function(index){
-        vm.index=index;
-       };
-        vm.getCardTime = function() {
-            if(typeof vm.options.projects != 'undefined'){
+        vm.setIndex = function (index) {
+            vm.index = index;
+        };
+        vm.getCardTime = function () {
+            if (typeof vm.options.projects != 'undefined') {
                 var newProject = [];
                 for (var i = 0; i < vm.options.projects.length; i++) {
                     newProject.push(vm.options.projects[i].id);
                 }
-            
+
                 vm.options.projects = newProject;
             }
             console.log(vm.options);
-            /*
-            if(vm.index==0){
-            CardsService.getCardLeadTime(vm.options).then(function(result) {
-                if (result.status === 201) {
-                   // $uibModalInstance.close(result.data);
-                }
-            }else if(vm.index==1){
-            CardsService.getCardColumnTime(vm.options).then(function(result) {
-                if (result.status === 201) {
-                   // $uibModalInstance.close(result.data);
-                }
-            });
+
+            if (vm.index == 0) {
+                CardsService.getCardLeadTime(vm.options).then(function (result) {
+                    if (result.status === 201) {
+                        // $uibModalInstance.close(result.data);
+                    }
+
+                })
+            } else if (vm.index == 1) {
+                CardsService.getCardColumnTime(vm.options).then(function (result) {
+                    if (result.status === 201) {
+                        // $uibModalInstance.close(result.data);
+                    }
+                });
+            } else if (vm.index == 2) {
+
+                console.log(vm.options);
+                /*CardsService.getCardColumnTime(vm.options).then(function (result) {
+                    if (result.status === 201) {
+                        // $uibModalInstance.close(result.data);
+                    }
+                });*/
             }
-            */
+
             vm.labels = [];
             vm.cardLeadTime = [];
             vm.cardLeadTime2 = [];
             vm.cardLeadTime3 = [];
             vm.leadTimeAvg = 0;
             for (var i = 0; i < 10; i++) {
-                vm.labels.push({"label":  i.toString() });
-                var tmp = Math.floor(Math.random() * 15) + 1 ;
-                vm.cardLeadTime.push({"value":  tmp.toString() });
+                vm.labels.push({"label": i.toString()});
+                var tmp = Math.floor(Math.random() * 15) + 1;
+                vm.cardLeadTime.push({"value": tmp.toString()});
                 vm.leadTimeAvg += tmp;
-                tmp = Math.floor(Math.random() * 15) + 1 ;
-                vm.cardLeadTime2.push({"value":  tmp.toString() });
-                tmp = Math.floor(Math.random() * 15) + 1 ;
-                vm.cardLeadTime3.push({"value":  tmp.toString() });
-                
+                tmp = Math.floor(Math.random() * 15) + 1;
+                vm.cardLeadTime2.push({"value": tmp.toString()});
+                tmp = Math.floor(Math.random() * 15) + 1;
+                vm.cardLeadTime3.push({"value": tmp.toString()});
+
             }
-            
+
             vm.leadTimeAvg = vm.leadTimeAvg / vm.cardLeadTime.length;
-            vm.category= vm.labels;
-            
+            vm.category = vm.labels;
+
             vm.dataset2 = [
-                {            
+                {
                     "seriesname": "Profit",
                     "renderas": "area",
                     "showvalues": "0",
@@ -181,7 +191,7 @@
                 },
                 {
                     "seriesname": "Actual Revenue",
-                    "data":vm.cardLeadTime
+                    "data": vm.cardLeadTime
                 },
                 {
                     "seriesname": "Projected Revenue",
@@ -190,8 +200,8 @@
                     "data": vm.cardLeadTime
                 }
             ];
-            vm.dataset2.push({"seriesname": "stolpec","renderas": "line","data": vm.cardLeadTime2});
-            vm.dataset2.push({"seriesname": "stolpec","renderas": "line","data": vm.cardLeadTime3});
+            vm.dataset2.push({"seriesname": "stolpec", "renderas": "line", "data": vm.cardLeadTime2});
+            vm.dataset2.push({"seriesname": "stolpec", "renderas": "line", "data": vm.cardLeadTime3});
             vm.updateGraph1();
         };
     }
